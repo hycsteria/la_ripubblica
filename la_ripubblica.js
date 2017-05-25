af9a858bdb619c6c83fff2e331110d3327d699c9
Chiara Broggi <broggichiara88@gmail.com>
2017-05-26 00:40:34 +0200

    Add some first code and Google Extension structure

--- la_ripubblica.js
+++ la_ripubblica.js
@@ -1,22 +1,46 @@
-cartoons = [
-{text_to_replace:"Trump", new_text:"PORKY", image:"http://www.ludicer.it/personaggi/porky-pig/porky.gif"},
-{text_to_replace:"Morata", new_text:"HERCULES", image:"http://images.clipartpanda.com/hercules-clip-art-hercules002.gif"},
-{text_to_replace:"Totti", new_text:"CENERENTOLA", image:""}
-];
+function substituteDummyForCartoons() {
 
-for (var i = 0; i < cartoons.length; i++){
+    cartoons = [{
+            text_to_replace: "Trump",
+            new_text: "PORKY",
+            image: "http://www.ludicer.it/personaggi/porky-pig/porky.gif"
+        },
+        {
+            text_to_replace: "Valentino Rossi",
+            new_text: "HERCULES",
+            image: "http://images.clipartpanda.com/hercules-clip-art-hercules002.gif"
+        },
+        {
+            text_to_replace: "Totti",
+            new_text: "CENERENTOLA",
+            image: "http://www.regnodisney.it/wp-content/uploads/2014/12/cinderella-disneyscreencaps.com-5423.jpg"
+        }
+    ];
 
-	text_to_replace = cartoons[i].text_to_replace;
-	text_new = cartoons[i].text_new;
-	image = cartoons[i].image;
 
-	article = jQuery('div:contains("' + text_to_replace + '")').closest("article").find("img");
-	article.attr("src", image);
+    for (var i = 0; i < cartoons.length; i++) {
 
-	article.children().each(function () {
-	    $(this).html( $(this).html().replace(/text_to_replace/g,text_new) );
-	})
+        text_to_replace = cartoons[i].text_to_replace;
+        text_new = cartoons[i].new_text;
+        image = cartoons[i].image;
+        var article_container = "";
+
+
+
+        article_container_array = jQuery('div:contains("' + text_to_replace + '")').each(function() {
+
+            article_container = $(this).closest("article");
+            if (article_container.html() != undefined) {
+
+                var re = new RegExp(text_to_replace, "g");
+                new_article_text = article_container.html().replace(re, text_new);
+                article_container.html(new_article_text);
+            }
+
+            article_image = article_container.find("img");
+            article_image.attr("src", image);
+        })
+
+    }
 
 };
-
-
