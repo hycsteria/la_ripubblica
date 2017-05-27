0cc8ea7d61ed7c062149f629a9b8299c5518f30c
Chiara Broggi <broggichiara88@gmail.com>
2017-05-27 15:23:38 +0200

    Remove tutorial test

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 // Called when the user clicks on the browser action.
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Turning ' + tab.url + ' red!');
+  chrome.browserAction.setBadgeText({text: "ON"});
+  //TODO: move jquery to lib folder
   chrome.tabs.executeScript(null, {file:'jquery-3.2.1.min.js'});
   chrome.tabs.executeScript({
     file: 'la_ripubblica.js'
